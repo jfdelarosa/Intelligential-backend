6727d14cd43bfd6bdf8034de990a29a04189663b
Juan Fernando <jfdelarosa@outlook.com>
2021-03-01 00:12:01 -0600

    feat: auth services

---
+++ src/services/authServices.js
@@ -0,0 +1,34 @@
+const { User } = require("../models");
+
+const authServices = {
+  register: async (body) => {
+    try {
+      return await User.create(body);
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  login: async ({ email, password }) => {
+    try {
+      const user = await User.findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        throw new Error("Invalid credentials");
+      }
+
+      const isMatch = await user.comparePasswords(password);
+
+      if (isMatch) {
+        return user;
+      }
+
+      throw new Error("Invalid credentials");
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+};
+
+module.exports = authServices;
