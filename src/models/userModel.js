1b668b5e39812c9427e8749b8ebc76447a2a1fe6
Juan Fernando <jfdelarosa@outlook.com>
2021-02-28 22:06:43 -0600

    feat: userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,15 +1,24 @@
 const UserModel = ({ sequelize, DataTypes }) => {
-  const User = sequelize.define("User", {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        default: "reader",
+      },
     },
-    role: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      default: "reader",
-    },
-  });
+    {}
+  );
 
   return User;
 };
